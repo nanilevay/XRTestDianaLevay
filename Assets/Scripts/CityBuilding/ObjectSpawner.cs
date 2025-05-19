93cc0236d9aba8cde124102454f4a6e9e8707c89
nanilevay <dianaalevay@gmail.com>
2025-05-19 20:23:44 +0100

    Add object spawner logic

---
+++ Assets/Scripts/CityBuilding/ObjectSpawner.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject EffectSpawner;
+
+    public Transform Parent;
+
+    // prefab test - move later
+    public void SpawnPrefab(BuildingData ObjectToSpawn, Vector3 SpawnPosition)
+    {
+        // Instantiate a new building on the correct tile assuming the rotation of the grid
+        GameObject Instant = Instantiate(ObjectToSpawn.objectPrefab, SpawnPosition, Parent.transform.rotation);
+
+        // Parent object to grid test - to be changed
+        Instant.transform.SetParent(Parent.transform, true);
+
+        EffectSpawner.SetActive(false);
+    }
+}
