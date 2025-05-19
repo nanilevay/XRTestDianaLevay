b007c1e5fc92c2853eebd3654b920de4f1fbe680
nanilevay <dianaalevay@gmail.com>
2025-05-19 20:41:46 +0100

    Update to building scriptable objects

--- Assets/Scripts/CityBuilding/BuildingData.cs
+++ Assets/Scripts/CityBuilding/BuildingData.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName ="Object Info")]
-public class CityObject : ScriptableObject
+[CreateAssetMenu(menuName ="Building Data")]
+public class BuildingData : ScriptableObject
 {
     public int objectId;
 
